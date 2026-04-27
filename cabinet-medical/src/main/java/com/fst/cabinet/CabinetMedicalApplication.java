ce79e5294a3b58977d0a955d3d5bcf4402837c9a
faiza <faiza@email.com>
2026-04-28 00:19:52 +0100

    ajout CabinetMedicalApplication

--- cabinet-medical/src/main/java/com/fst/cabinet/CabinetMedicalApplication.java
+++ cabinet-medical/src/main/java/com/fst/cabinet/CabinetMedicalApplication.java
@@ -1,13 +1,17 @@
 package com.fst.cabinet;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure
+    .SpringBootApplication;
+import org.springframework.scheduling.annotation
+    .EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CabinetMedicalApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CabinetMedicalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(
+            CabinetMedicalApplication.class, args);
+    }
 }
+
